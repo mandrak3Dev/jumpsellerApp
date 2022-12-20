68b246c5c05b2f84b8237b37349d7de4cd9dcc03
Mau Orozco <mau@192.168.1.2>
2022-12-20 00:06:10 -0500

    switch to nextjs

---
+++ pages/index.tsx
@@ -0,0 +1,39 @@
+// import Head from "next/head";
+// import styles from "../styles/Home.module.css";
+import { useEffect } from "react";
+import axios from "axios";
+
+export const Home = () => {
+  useEffect(() => {
+    const httpCall = async () => {
+      const client_id = process.env.NEXT_PUBLIC_CLIENT_ID;
+      const client_secret = process.env.NEXT_PUBLIC_CLIENT_SECRET;
+      const redirect_uri = process.env.NEXT_PUBLIC_REDIRECT_URI;
+      const code = new URLSearchParams(window.location.search).get("code");
+      const grant_type = "authorization_code";
+      const authUrl = "https://accounts.jumpseller.com/oauth/token";
+
+      try {
+        const codeRequest = await axios.post(authUrl, {
+          client_id,
+          client_secret,
+          redirect_uri,
+          code,
+          grant_type,
+        });
+        localStorage.setItem("tokenData", JSON.stringify(codeRequest.data));
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    httpCall();
+  }, []);
+
+  return (
+    <div>
+      <h1>Hello world!</h1>
+    </div>
+  );
+};
+
+export default Home;
