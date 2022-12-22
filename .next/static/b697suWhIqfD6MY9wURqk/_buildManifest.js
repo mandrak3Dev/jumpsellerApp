abf35a19a065a6c6f325da04b3c9f92d56372189
Mau Orozco <mau@NCO-NTB-021.local>
2022-12-21 21:16:04 -0500

    first commit

---
+++ .next/static/b697suWhIqfD6MY9wURqk/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/594-da3882d53abc6ca3.js","static/chunks/pages/index-5001664b6d7be789.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
